e99c57a7c6bdc778bed7ef7efab0d759bc908f08
donovan_bidlack <donovan.bidlack@tdwilliamson.com>
2021-04-28 13:29:26 -0600

    Works well now, not my neatest work, but just thrown together

--- rawDataInterpolation.py
+++ rawDataInterpolation.py
@@ -7,7 +7,7 @@ ODOB = 1
 ODOC = 2
 ODO_MIN = [51,51,53]
 ODO_MAX = [971,967,966]
-ROLLOVER_DIFF = 40 
+ROLLOVER_DIFF = 1 
 previousDiff = [0,0,0]
 
 df = pd.read_csv('1.csv')
@@ -39,37 +39,20 @@ def interpolateData( curIdx, prevIdx ):
             if odoADiff > 0:
                 df.at[prevIdx+i, 'WHEEL_A'] = int(df.at[prevIdx, 'WHEEL_A']) + int(i*odoADiffPerSample)
             else:
-                if int(df.at[prevIdx, 'WHEEL_A']) + previousDiff[ODOA]*i < ODO_MAX[ODOA] and int(df.at[prevIdx, 'WHEEL_A']) + previousDiff[ODOA]*i > 0 :
-                    df.at[prevIdx+i, 'WHEEL_A'] = int(df.at[prevIdx+i-1, 'WHEEL_A']) + previousDiff[ODOA]
-                else:
-                    if int(df.at[prevIdx+i-1, 'WHEEL_A']) - ROLLOVER_DIFF > ODO_MIN[ODOA]:
-                        df.at[prevIdx+i, 'WHEEL_A'] = int(df.at[prevIdx+i-1, 'WHEEL_A']) - ROLLOVER_DIFF
-                    else:
-                        df.at[prevIdx+i, 'WHEEL_A'] = ODO_MAX[ODOA] + previousDiff[ODOA]*i
+                df.at[prevIdx+i, 'WHEEL_A'] = int(df.at[prevIdx+i-1, 'WHEEL_A']) - ROLLOVER_DIFF
 
         if( int(df.at[prevIdx, 'WHEEL_B']) + int(df.at[curIdx, 'WHEEL_B']) != 0 ):
             if odoBDiff > 0:
                 df.at[prevIdx+i, 'WHEEL_B'] = int(df.at[prevIdx, 'WHEEL_B']) + int(i*odoBDiffPerSample)
             else:
-                if int(df.at[prevIdx, 'WHEEL_B']) + previousDiff[ODOB]*i < ODO_MAX[ODOB] and int(df.at[prevIdx, 'WHEEL_B']) + previousDiff[ODOB]*i > 0 :
-                    df.at[prevIdx+i, 'WHEEL_B'] = int(df.at[prevIdx+i-1, 'WHEEL_B']) + previousDiff[ODOB]
-                else:
-                    if int(df.at[prevIdx+i-1, 'WHEEL_B']) - ROLLOVER_DIFF > ODO_MIN[ODOB]:
-                        df.at[prevIdx+i, 'WHEEL_B'] = int(df.at[prevIdx+i-1, 'WHEEL_B']) - ROLLOVER_DIFF
-                    else:
-                        df.at[prevIdx+i, 'WHEEL_B'] = ODO_MAX[ODOB] + previousDiff[ODOB]*i
+                df.at[prevIdx+i, 'WHEEL_B'] = int(df.at[prevIdx+i-1, 'WHEEL_B']) - ROLLOVER_DIFF
 
         if( int(df.at[prevIdx, 'WHEEL_C']) + int(df.at[curIdx, 'WHEEL_C']) != 0 ):
             if odoCDiff > 0:
                 df.at[prevIdx+i, 'WHEEL_C'] = int(df.at[prevIdx, 'WHEEL_C']) + int(i*odoCDiffPerSample)
             else:
-                if int(df.at[prevIdx, 'WHEEL_C']) + previousDiff[ODOC]*i < ODO_MAX[ODOC] and int(df.at[prevIdx, 'WHEEL_C']) + previousDiff[ODOC]*i > 0 :
-                    df.at[prevIdx+i, 'WHEEL_C'] = int(df.at[prevIdx+i-1, 'WHEEL_C']) + previousDiff[ODOC]
-                else:
-                    if int(df.at[prevIdx+i-1, 'WHEEL_C']) - ROLLOVER_DIFF > ODO_MIN[ODOC]:
-                        df.at[prevIdx+i, 'WHEEL_C'] = int(df.at[prevIdx+i-1, 'WHEEL_C']) - ROLLOVER_DIFF
-                    else:
-                        df.at[prevIdx+i, 'WHEEL_C'] = ODO_MAX[ODOC] + previousDiff[ODOC]*i
+                df.at[prevIdx+i, 'WHEEL_C'] = int(df.at[prevIdx+i-1, 'WHEEL_C']) - ROLLOVER_DIFF
+
 
         i+=1
 
@@ -80,13 +63,54 @@ def interpolateData( curIdx, prevIdx ):
     if odoCDiff > 0:
         previousDiff[ODOC] = odoCDiff
 
+#OVERSAMPLE TICK DATA TO ODO SAMPLE DATA
 previousIndex = 0
 for index, row in df.iterrows():
     if row.sum() != 0:
         interpolateData( int(index), previousIndex )
         previousIndex = int(index)
 
+#CORRECT ROLLOVERS
+startOfRollover = [0,0,0]
+for index, row in df.iterrows():
+    if index > 2:
+        currentValue = df.at[index, 'WHEEL_A']
+        previousValue = df.at[index-1, 'WHEEL_A']
+        twoValuesAgo = df.at[index-2, 'WHEEL_A']
 
+        if currentValue < previousValue and previousValue > twoValuesAgo:
+            startOfRollover[ODOA] = index-1
+        elif currentValue > previousValue and previousValue < twoValuesAgo:
+            numberOfValuesToChange = index-2 - startOfRollover[ODOA]
+            changeInValue = int((df.at[startOfRollover[ODOA], 'WHEEL_A'] - df.at[index-1, 'WHEEL_A']) / (numberOfValuesToChange+1))
+            for i in range(numberOfValuesToChange):
+                df.at[index-2-i, 'WHEEL_A'] = df.at[index-1-i, 'WHEEL_A'] + changeInValue
+
+        currentValue = df.at[index, 'WHEEL_B']
+        previousValue = df.at[index-1, 'WHEEL_B']
+        twoValuesAgo = df.at[index-2, 'WHEEL_B']
+
+        if currentValue < previousValue and previousValue > twoValuesAgo:
+            startOfRollover[ODOB] = index-1
+        elif currentValue > previousValue and previousValue < twoValuesAgo:
+            numberOfValuesToChange = index-2 - startOfRollover[ODOB]
+            changeInValue = int((df.at[startOfRollover[ODOB], 'WHEEL_B'] - df.at[index-1, 'WHEEL_B']) / (numberOfValuesToChange+1))
+            for i in range(numberOfValuesToChange):
+                df.at[index-2-i, 'WHEEL_B'] = df.at[index-1-i, 'WHEEL_B'] + changeInValue
+
+        currentValue = df.at[index, 'WHEEL_C']
+        previousValue = df.at[index-1, 'WHEEL_C']
+        twoValuesAgo = df.at[index-2, 'WHEEL_C']
+
+        if currentValue < previousValue and previousValue > twoValuesAgo:
+            startOfRollover[ODOC] = index-1
+        elif currentValue > previousValue and previousValue < twoValuesAgo:
+            numberOfValuesToChange = index-2 - startOfRollover[ODOC]
+            changeInValue = int((df.at[startOfRollover[ODOC], 'WHEEL_C'] - df.at[index-1, 'WHEEL_C']) / (numberOfValuesToChange+1))
+            for i in range(numberOfValuesToChange):
+                df.at[index-2-i, 'WHEEL_C'] = df.at[index-1-i, 'WHEEL_C'] + changeInValue
+
+#CONVERT FROM 10-bit to 16-bit
 for index, row in df.iterrows():
     df.at[index, 'WHEEL_A'] = int(row['WHEEL_A']) << 6
     df.at[index, 'WHEEL_B'] = int(row['WHEEL_B']) << 6
